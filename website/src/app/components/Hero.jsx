cde63617d6b6e7100feb38d038b30d4d90a0d24e
Rockstar100 <boyrockstar100@gmail.com>
2024-05-08 17:38:31 +0530

    Changes

--- website/src/app/components/Hero.jsx
+++ website/src/app/components/Hero.jsx
@@ -8,14 +8,14 @@ import "@/app/Style/hero.css";
 const Hero = () => {
   return (
     <div className="bg-home-page-back bg-cover bg-no-repeat">
-       <div className="bg-radial-gradient flex justify-center overflow-hidden lg:h-[fit-content] md:h-[fit-content] ">
+      <div className="bg-radial-gradient flex justify-center overflow-hidden lg:h-[80vh] md:h-[80vh]">
         <Motionframe/>
       </div>
       <div className="h-[750px] bg-white-gradient flex flex-col items-center justify-center">
         <header className="font-Pattaya  text-[#0C6242] text-[52px] max-sm:text-[29px] sm:max-md:text-[42px] md:pt-20">
           Why Anahata Connections?
         </header>
-        <header className="font-Satisfy  text-4xl pt-10 max-sm:text-[20px]  sm:max-md:text-[25px] w-[90%] sm:w-[80%] md:w-[70%]  text-center ">
+        <header className="text-black font-Satisfy  text-4xl pt-10 max-sm:text-[20px]  sm:max-md:text-[25px] w-[90%] sm:w-[80%] md:w-[70%]  text-center ">
           Welcome to Anahata Connections, where love intertwines with
           spirituality. Explore meaningful connections grounded in yogic science
           and the wisdom of the heart chakra. Our platform unites kindred spirits
