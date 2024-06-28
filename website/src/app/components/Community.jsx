0ea3bda0ca8c37dd380e62d6d08bd5c00309067d
Miran Hamid <miranhamid2002@gmail.com>
2024-06-28 18:41:54 +0530

    update community chat componentn

--- website/src/app/components/Community.jsx
+++ website/src/app/components/Community.jsx
@@ -31,6 +31,25 @@ const Community = () => {
     return <div>Loading...</div>;
   }
 
+  const images = [
+    {
+      src: "https://cdn.builder.io/api/v1/image/assets/TEMP/5b98072059023c1cb512919babbfd7091b1e6f4c30e17685f334605454329f07?apiKey=b0951f87dee4449fa252b5f3cfc3b012&",
+      message: ` ${communityData.text1}`,
+    },
+    {
+      src: "https://cdn.builder.io/api/v1/image/assets/TEMP/3b6276de1c719c39f7ef529931d0f9363baf8c463fed31657c275cc214691980?apiKey=b0951f87dee4449fa252b5f3cfc3b012&",
+      message: ` ${communityData.text2}`,
+    },
+    {
+      src: "https://cdn.builder.io/api/v1/image/assets/TEMP/00bd5a9f903d195224eb1daadeb0557f1d7ac99bc58a161a20213f7e4037c86b?apiKey=b0951f87dee4449fa252b5f3cfc3b012&",
+      message: ` ${communityData.text3}`,
+    },
+    {
+      src: "https://cdn.builder.io/api/v1/image/assets/TEMP/3b6276de1c719c39f7ef529931d0f9363baf8c463fed31657c275cc214691980?apiKey=b0951f87dee4449fa252b5f3cfc3b012&",
+      message: ` ${communityData.text4}`,
+    },
+  ];
+
   return (
     <div className="w-[100%] flex flex-col justify-center items-center mt-[50px] mb-[50px] mobile:mt-[100px] gap-[50px]   mobile:mb-[100px]">
       <header className="font-Pattaya flex items-center justify-center text-[#094C3B] text-[25px] mobile:text-[45px]">
@@ -46,7 +65,31 @@ const Community = () => {
           ))}
         </div>
 
-        <ChatBox />
+        <div className="relative flex flex-col w-fit gap-[10px]">
+          {images.map(({ message, src }, index) => {
+            return (
+              <div
+                key={index}
+                className={`w-[100%] h-min-fit flex  justify-center ${
+                  (index === 1 || index === 3) &&
+                  "mobile:translate-x-[30%] tab:translate-x-[60%]"
+                }`}
+              >
+                <img
+                  src={src}
+                  width={100}
+                  height={60}
+                  className="w-[100%] h-min-fit"
+                  alt="image"
+                />
+                <p className="absolute flex justify-center items-center text-white text-[16px] box-border p-[6px] mobile:p-[10px]">
+                  {message}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+        {/* <ChatBox  /> */}
       </div>
     </div>
   );
