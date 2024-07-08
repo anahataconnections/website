16da5f546129b23dba49a41f214c1c3eca72bb82
Miran Hamid <miranhamid2002@gmail.com>
2024-07-08 17:11:54 +0530

    add responsiveness

--- website/src/app/components/LetsGet.jsx
+++ website/src/app/components/LetsGet.jsx
@@ -30,8 +30,8 @@ const LetsGet = () => {
 
   return (
     <main className="px-5 custom2:h-[30rem] pt-24">
-      <div className="grid grid-cols-2 gap-x-10">
-        <div className="flex flex-col justify-center items-center gap-5">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-x-10">
+        <div className="flex flex-col justify-center items-center gap-5 text-center">
           <span className="text-emerald-900 font-Satisfy text-[60px]">
             {plans.content[0].children[0].text}
           </span>
@@ -40,7 +40,7 @@ const LetsGet = () => {
             {plans.content[1].children[0].text}
           </span>
         </div>
-        <div className="text-lg font-medium flex flex-col justify-center">
+        <div className="text-lg font-medium flex flex-col justify-center mb-5 lg:mb-0">
           {plans.content.map((item, index) => {
             // Check if the current item is a paragraph
             if (item.type === "paragraph") {
