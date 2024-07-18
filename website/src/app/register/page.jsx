6e645705c50596d4e06369b9ab45d227de8dc4a9
Miran Hamid <miranhamid2002@gmail.com>
2024-07-18 07:22:10 +0530

    updates

--- website/src/app/register/page.jsx
+++ website/src/app/register/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import Image from "next/image";
-import img from "@/helpers/images/register.png";
+import img from "@/helpers/images/registerbanner.png";
 import logo from "@/helpers/images/logo.png";
 import Link from "next/link";
 
@@ -30,7 +30,7 @@ const page = () => {
       }
 
       const data = await response.json();
-      
+
       localStorage.setItem("token", data.token);
 
       alert("Registration successful! Redirecting to Overview...");
@@ -41,43 +41,32 @@ const page = () => {
   };
 
   return (
-    <main className="pt-14 bg-emerald-900 shadow-sm w-full z-[11]">
-      <div className="flex gap-5 max-md:flex-col max-md:gap-0">
-        <section className="flex flex-col w-1/2 ">
-          <Image
-            src={img}
-            alt="Decorative image"
-            width={500}
-            height={943}
-            className=""
-          />
-        </section>
-        <section className="flex flex-col items-center z-[11] pt-14">
+    <main className="bg-emerald-900 shadow-sm w-full z-[11]">
+      <div className="flex justify-center items-center pb-40">
+        <section className="flex flex-col items-center justify-center w-full z-[11] pt-14">
           <form
             onSubmit={handleSubmit}
-            className="flex flex-col text-2xl leading-8 text-neutral-200 max-md:mt-10 max-md:max-w-full"
+            className="relative flex flex-col justify-center items-center text-2xl leading-8 text-neutral-200 max-md:mt-10 w-full"
           >
-            <div className=" flex flex-col justify-center items-center mb-10">
+            <div className=" flex flex-col justify-center items-center">
               <Image
                 src={logo}
                 alt="Logo"
                 width={279}
                 height={372}
-                className="w-60 h-60 object-cover"
+                className="object-cover"
               />
-
-              <div className="font-EvaMayasari text-[60px] text-center ">
-                Anahata Connections
-              </div>
             </div>
 
-            <div className="flex flex-col gap-y-6 text-base">
+            <div className="relative flex flex-col gap-y-6 text-base z-[11]">
+              <Image src={img} className="absolute scale-[2] -z-10" />
+
               <input
                 type="text"
                 name="name"
                 id="name"
                 placeholder="john"
-                className="border-[#FFFFFF] border-2 rounded-lg py-2.5 bg-transparent text-white px-2.5 focus:outline-none"
+                className="border-[#FFFFFF] border-2 rounded-lg py-2.5 bg-transparent text-white px-2.5 focus:outline-none w-96"
               />
               <input
                 type="email"
@@ -94,7 +83,7 @@ const page = () => {
                 className="border-[#FFFFFF] border-2 rounded-lg py-2.5 bg-transparent text-white px-2.5 focus:outline-none"
               />
             </div>
-            <button className="self-center px-14 py-3 mt-10 text-xl font-bold tracking-wide text-center text-emerald-900 whitespace-nowrap bg-white rounded-[32px] max-md:px-5 max-md:mt-10 hover:scale-105 transition ease-linear duration-200">
+            <button className="self-center px-14 py-3 mt-10 text-xl font-bold tracking-wide text-center text-emerald-900 whitespace-nowrap bg-white rounded-[32px] max-md:px-5 max-md:mt-10 hover:scale-105 transition ease-linear duration-200 cursor-pointer z-[11]">
               Signup
             </button>
           </form>
